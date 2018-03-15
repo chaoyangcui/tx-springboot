3f0ea039fbdfa9c4145656df00a37370d286cf76
eric <cui27387@163.com>
2018-03-16 00:10:50 +0800

    add redisˆ

--- src/main/java/com/tx/txspringboot/TxSpringbootApplication.java
+++ src/main/java/com/tx/txspringboot/TxSpringbootApplication.java
@@ -2,14 +2,24 @@ package com.tx.txspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @SpringBootApplication
 @Import({WebConfig.class})
 public class TxSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TxSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TxSpringbootApplication.class, args);
+    }
 
+    @Bean
+    Jedis jedis() {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+        return pool.getResource();
+        // return new Jedis("localhost", 6379);
+    }
 }
