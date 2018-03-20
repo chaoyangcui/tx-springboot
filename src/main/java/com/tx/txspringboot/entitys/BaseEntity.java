cd5a63070e21c5a478445a422e8c2b382085b603
eric <cui27387@163.com>
2018-03-20 23:01:02 +0800

    add serial test

---
+++ src/main/java/com/tx/txspringboot/entitys/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.tx.txspringboot.entitys;
+
+/**
+ * Base 类
+ *
+ * <p>Created by Intellij IDEA.
+ *
+ * @author Eric Cui
+ * @since 2018/3/20 22:37
+ */
+public class BaseEntity {
+    private String common;
+
+    public String getCommon() {
+        return common;
+    }
+
+    public void setCommon(String common) {
+        this.common = common;
+    }
+}
